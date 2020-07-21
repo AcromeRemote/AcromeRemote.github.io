763728952966fb2140ac8e429a85ce79283bb999
AcromeRemote <68596432+AcromeRemote@users.noreply.github.com>
2020-07-21 21:05:51 +0300

    Update config.js

--- ckeditor_4.14.1_full/ckeditor/config.js
+++ ckeditor_4.14.1_full/ckeditor/config.js
@@ -4,9 +4,11 @@
  */
 
 CKEDITOR.editorConfig = function( config ) {
+	CKFinder.setupCKEditor();
+	CKEDITOR.replace( 'editor1' );
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
-	var editor = CKEDITOR.replace( 'editor1' );
-	CKFinder.setupCKEditor( editor );
+	//var editor = CKEDITOR.replace( 'editor1' );
+	//CKFinder.setupCKEditor( editor );
 };
