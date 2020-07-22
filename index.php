a844a21b566641ee96a9a848ba94dea629c83b54
AcromeRemote <68596432+AcromeRemote@users.noreply.github.com>
2020-07-22 17:05:37 +0300

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+        <head>
+                <meta charset="utf-8">
+                <title>CKEditor</title>
+                <script src="ckeditor/ckeditor.js"></script>
+		<script src="ckfinder/ckfinder.js"></script>
+        </head>
+        <body>
+                <textarea name="editor1"></textarea>
+                <script>
+                   	var editor = CKEDITOR.replace( 'editor1' );
+			CKFinder.setupCKEditor( editor );
+                </script>
+        </body>
+</html>
